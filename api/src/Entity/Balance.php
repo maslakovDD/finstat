73cc200f04c4d2055c95d98b669b4e0f1038fecb
Dmytro Maslakov <ipz212_mdd@student.ztu.edu.ua>
2024-01-30 19:28:09 +0200

    added balance crud

--- api/src/Entity/Balance.php
+++ api/src/Entity/Balance.php
@@ -2,10 +2,41 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\BalanceRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
+#[
+    ApiResource(
+        collectionOperations: [
+            "get"  => [
+                "method" => "GET",
+                "normalization_context" => ['groups' => ['get:collection:balance']]
+            ],
+            "post" => [
+                "method"                  => "POST",
+                "denormalization_context" => ['groups' => ['post:collection:balance']],
+                "normalization_context"   => ["groups" => ["get:item:balance"]],
+            ]
+        ],
+        itemOperations: [
+            "get"    => [
+                "method"                => "GET",
+                "normalization_context" => ['groups' => ['get:item:balance']],
+            ],
+            "patch"  => [
+                "method"                  => "PATCH",
+                "denormalization_context" => ['groups' => ["patch:item:balance"]],
+                "normalization_context"   => ['groups' => ['get:item:balance']],
+            ],
+            "delete" => [
+                "method"   => "DELETE",
+            ]
+        ]
+    )
+]
 #[ORM\Entity(repositoryClass: BalanceRepository::class)]
 class Balance
 {
@@ -16,18 +47,34 @@ class Balance
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups([
+        "get:item:balance",
+        "get:collection:balance"
+    ])]
     private ?int $id = null;
 
     /**
      * @var string|null
      */
     #[ORM\Column(length: 255)]
+    #[Groups([
+        "get:item:balance",
+        "get:collection:balance",
+        "post:collection:balance",
+        "patch:item:balance"
+    ])]
     private ?string $title = null;
 
     /**
      * @var string|null
      */
     #[ORM\Column(type: Types::DECIMAL, precision: 5, scale: 2)]
+    #[Groups([
+        "get:item:balance",
+        "get:collection:balance",
+        "post:collection:balance",
+        "patch:item:balance"
+    ])]
     private ?string $amount = null;
 
     /**
@@ -35,6 +82,12 @@ class Balance
      */
     #[ORM\ManyToOne(inversedBy: 'balances')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups([
+        "get:item:balance",
+        "get:collection:balance",
+        "post:collection:balance",
+        "patch:item:balance"
+    ])]
     private ?Currency $currency = null;
 
     /**
@@ -42,6 +95,10 @@ class Balance
      */
     #[ORM\ManyToOne(inversedBy: 'balances')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups([
+        "get:item:balance",
+        "get:collection:balance"
+    ])]
     private ?User $user = null;
 
     /**
